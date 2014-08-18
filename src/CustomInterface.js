adeac44fc572e84739959cfbaa5c52dfab336201
Helder <he7d3r@gmail.com>
2014-08-18 18:11:23 -0300

    Inform the license: CC BY-SA 3.0

--- src/CustomInterface.js
+++ src/CustomInterface.js
@@ -2,7 +2,8 @@
  * Allow the use of a CSS3 selector such as "html:not(.mw) " to define an interface customization which may be disable/enabled by one click
  *
  * @see: [[User:Helder.wiki/vector.css]] for examples
- * @author: [[User:Helder.wiki]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/CustomInterface.js]] ([[File:User:Helder.wiki/Tools/CustomInterface.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
