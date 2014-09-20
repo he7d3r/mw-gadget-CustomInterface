a93cd4d7e4a2195c8077e03e6fc674109a45fb7f
Helder <he7d3r@gmail.com>
2014-09-20 11:22:27 -0300

    Remove tracking link (only useful on wiki)

--- src/CustomInterface.js
+++ src/CustomInterface.js
@@ -4,7 +4,6 @@
  * @see: [[User:Helder.wiki/vector.css]] for examples
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/CustomInterface.js]] ([[File:User:Helder.wiki/Tools/CustomInterface.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
