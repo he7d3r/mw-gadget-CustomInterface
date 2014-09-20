1afa7338d05d9a16a00994008b782911a4e070f6
Helder <he7d3r@gmail.com>
2014-09-20 13:39:50 -0300

    Update link to examples

--- src/CustomInterface.js
+++ src/CustomInterface.js
@@ -1,7 +1,7 @@
 /**
  * Allow the use of a CSS3 selector such as "html:not(.mw) " to define an interface customization which may be disable/enabled by one click
  *
- * @see: [[User:Helder.wiki/vector.css]] for examples
+ * @see: [[m:User:He7d3r/global.css]] for examples
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
